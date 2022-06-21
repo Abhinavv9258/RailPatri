e9b552aea8d862dd9151bcb6e5a52b65aa27b86d
Abhinavv9258 <Abhinavv218@gmail.com>
2022-06-21 17:42:17 +0530

    WriteCommit

---
+++ connection/connection.php
@@ -0,0 +1,13 @@
+<?php
+$server_name = "localhost";
+$uname = "root";
+// $pass = "Fuzzi@#4567";
+// $pass = "";
+$database_name = "railway_management";
+$connection = mysqli_connect($server_name, $uname);
+//now check the connection
+if (!$connection) {
+die("Please try again! Connection Failed:" . mysqli_connect_error());
+}
+mysqli_select_db($connection, $database_name);
+?>
